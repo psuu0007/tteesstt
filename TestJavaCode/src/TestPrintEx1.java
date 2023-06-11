8f79c954c640bfff7d312996ec1ecbbaf5cd56d4
psuu0007@naver.com <psuu0007@naver.com>
2023-06-11 23:25:06 +0900

    일단 main에 올린다 다른 애는 이름이 master이다

---
+++ TestJavaCode/src/TestPrintEx1.java
@@ -0,0 +1,21 @@
+import java.text.Format;
+
+public class TestPrintEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int octNum = 010;
+		
+		System.out.println(octNum);
+		
+		int num = 8;
+		
+		System.out.printf("num=%#o\n", num);
+		System.out.printf("num=%#x\n", num);
+		
+		int hexNum = Integer.parseInt("0a", 16);
+		System.out.println(hexNum);
+	}
+
+}
