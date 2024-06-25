76441413ae9dfd3b09af698dd99b6ad29612a3f8
psuu0007 <psuu0007@naver.com>
2024-06-25 22:40:57 +0900

    Reapply "음 역시 커밋해야 머지가 되는건가?"
    This reverts commit 097d29226781c1b6b440dabdc8e95102d90aacc4.

--- zzzgitTest/src/test/beta/Test1.java
+++ zzzgitTest/src/test/beta/Test1.java
@@ -5,4 +5,10 @@ public class Test1 {
 	
 	
 //	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
+	public static void main(String[] args) {
+		System.out.println("맞다 이런 느낌인거 같다 브랜치가 ");
+		System.out.println("일단 이게 필요하다고 생각하자");
+		System.out.println("main에서 이것도 있을까?");
+		System.out.println();
+	}
 }
