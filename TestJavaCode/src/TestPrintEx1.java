8df9c8d8d649a835a8ec98cae0d6e0bf47a6bf2f
psuu0007 <psuu0007@naver.com>
2024-06-25 22:39:22 +0900

    revert 해보는 중
    충돌 해결하고 여기서 올리면 되돌려 진건가?

--- TestJavaCode/src/TestPrintEx1.java
+++ TestJavaCode/src/TestPrintEx1.java
@@ -1,3 +1,4 @@
+import java.text.Format;
 
 public class TestPrintEx1 {
 
@@ -16,6 +17,8 @@ public class TestPrintEx1 {
 		int hexNum = Integer.parseInt("0a", 16);
 		System.out.println(hexNum);
 		
+		System.out.println("이 내용 취소될까?");
+
 	}
 
 }
