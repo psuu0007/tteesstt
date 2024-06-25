055a956898cbab21995b22f60d0c804773eb21e3
psuu0007 <psuu0007@naver.com>
2024-06-25 22:49:37 +0900

    아 뭐야? 커밋 정도는 충돌 이런거 없이 그냥 리셋되나 보네
    이러면 다시 한글이 깨질려나?

--- zzzgitTest/src/test/beta/Test1.java
+++ zzzgitTest/src/test/beta/Test1.java
@@ -1,14 +1,19 @@
 package test.beta;
 
+import java.util.Date;
+
 public class Test1 {
 
 	
 	
 //	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
 	public static void main(String[] args) {
-		System.out.println("맞다 이런 느낌인거 같다 브랜치가 ");
+		System.out.println("맞다 이런 느낌인거 같다 브랜치가");
 		System.out.println("일단 이게 필요하다고 생각하자");
-		System.out.println("main에서 이것도 있을까?");
-		System.out.println();
+		System.out.println("왜 글자가 깨지냐? 신기하네");
+		System.out.println("tired");
+		System.out.println(new Date());
+		
+		System.out.println("Tue Jun 25 22:45:10 KST 2024");
 	}
 }
